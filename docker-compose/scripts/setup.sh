e283544a7fc5a55e94352b0c1f36c106d5d52bf2
Mic Hussey <mic@confluent.io>
2020-03-06 11:02:30 +0100

    Updated connector versions and added MongoDB

--- docker-compose/scripts/setup.sh
+++ docker-compose/scripts/setup.sh
@@ -3,4 +3,4 @@
 confluent-hub install --component-dir ./connect-plugins/ --no-prompt jcustenborder/kafka-connect-spooldir:2.0.43
 confluent-hub install --component-dir ./connect-plugins/ --no-prompt wepay/kafka-connect-bigquery:1.3.0
 confluent-hub install --component-dir ./connect-plugins/ --no-prompt debezium/debezium-connector-postgresql:1.0.0
-confluent-hub install --component-dir ./connect-plugins/ --no-prompt  hpgrahsl/kafka-connect-mongodb:1.3.1
+confluent-hub install --component-dir ./connect-plugins/ --no-prompt hpgrahsl/kafka-connect-mongodb:1.3.1
